bb3e6ff08c2b8ca592c60db2db708b9223afb2af
ManuelPrhyme <manuelbagoole@gmail.com>
2026-02-14 19:50:32 +0300

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,16 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import {useState, useEffect} from 'react'
 import {BounceLoader, PropagateLoader} from "react-spinners"
-import { dataSlice } from 'ethers'
+import { dataSlice, formatUnits } from 'ethers'
+import {formatEther} from 'viem'
 
 
 function App(){
 
 
   const [data,setData] = useState()
-  const [walletAddress, setWallet] = useState("0x780751D1c63eFeDB54332148552D0a1EbE1d303a")
+  const [walletAddress, setWallet] = useState("0x0f85f1523666118eB752eec4a6f763776F4b5693")
+  const [entireData, setEntireData] = useState()
 
   useEffect(()=>{
 
@@ -22,17 +24,25 @@ function App(){
     // }
 
     //
-
-     fetch(`https://api.covalenthq.com/v1/bsc-mainnet/address/${walletAddress}/balances_v2/?quote-currency=USD`,{
+// setTimeout(()=>{
+  
+     fetch(`https://api.sim.dune.com/v1/evm/activity/${walletAddress}?limit=40&chain_ids=1135`,{
       method:"GET",
       headers:{
-        Authorization: "Bearer cqt_rQMQxDm7BxcJ7yGcGRVgWKmYkBFJ"
+        // Authorization: "Bearer cqt_rQMQxDm7BxcJ7yGcGRVgWKmYkBFJ"
+        'X-Sim-Api-Key': "sim_gSCj2pv4LwopMJFCS3kctaHkG9mUYgV9"
       }
      })
     .then((stream)=>stream.json())
-    .then((dataInfo)=>{setData(dataInfo.data.items[1].pretty_quote_24h);
-      console.log(dataInfo)
+    .then((dataInfo)=>{
+      // setEntireData(dataInfo.activity);
+      setData(dataInfo.activity);
+      console.log(dataInfo.activity)
     })
+    .catch((err)=>{
+      console.log("The error is ",err)
+    })
+// },6500)
 
      
   },[])
@@ -40,10 +50,26 @@ function App(){
   return (
     <div>
    
-    <h1>Chuck Norris Jokes</h1>
-    <p>Chain Names</p>
-    <h3>{data}</h3>
+    <h1>API Requests</h1>
+    <h3>Address</h3>
+    {/* <div>{
+      entireData ? (<h3>{entireData.data.items[0].pretty_quote}</h3>) : (<p>Loading ...</p>)
+      }
+    </div> */}
+    
     {/* <img src={data} alt="The logo" /> */}
+
+    <div>
+      {
+        !data ? (<PropagateLoader />) : (
+          <ul>
+            {
+              data.map((element,index) => (<li key={index}>{element.asset_type} | {element.type} | { element.asset_type == 'native' ? formatEther(element.value.toString()) : formatUnits(element.value, 18)} | {element.block_time}</li>))
+            }
+          </ul>
+        )
+      }
+    </div>
 
     {/* <div>
       {data.map((chain, index) => (
